2f3369024b53cedca7a23ae9c3e1dafe29d6b5c7
Caleb <scornsaber@gmail.com>
2014-06-13 21:20:58 -0500

    now chops all logs above chopped log.

--- src/treemauler/TreeMauler.java
+++ src/treemauler/TreeMauler.java
@@ -2,16 +2,21 @@ package treemauler;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TreeMauler extends JavaPlugin implements Listener {
 	public static Logger log = Logger.getLogger("Minecraft");
 
 	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
 		log.info("[TreeMauler] Start up.");
 	}
 
@@ -21,6 +26,32 @@ public class TreeMauler extends JavaPlugin implements Listener {
 
 	public void onDisable() {
 		log.info("[TreeMauler] Server stopping.");
+	}
+
+	@EventHandler
+	public void blockBreakListener(BlockBreakEvent event) {
+		log.info("[TreeMauler] Block broken:  " + event.getBlock().getType());
+		if (event.getBlock().getType() == Material.LOG) {
+			if ((event.getPlayer().getItemInHand().getType() == Material.WOOD_AXE)
+					|| (event.getPlayer().getItemInHand().getType() == Material.STONE_AXE)
+					|| (event.getPlayer().getItemInHand().getType() == Material.IRON_AXE)
+					|| (event.getPlayer().getItemInHand().getType() == Material.GOLD_AXE)
+					|| (event.getPlayer().getItemInHand().getType() == Material.DIAMOND_AXE)) {
+				Location loc = event.getBlock().getLocation();
+				loc.setY(1 + loc.getBlockY());
+				breakLog(loc);
+				log.info("[TreeMauler] Block broken.");
+
+			}
+		}
+	}
+
+	public void breakLog(Location loc) {
+		if (loc.getBlock().getType() == Material.LOG) {
+			loc.getBlock().breakNaturally();
+			loc.setY(1 + loc.getBlockY());
+			breakLog(loc);
+		}
 	}
 
 	public boolean onCommand(CommandSender sender, Command command,
